21293515a6274c781bb3a0a30a8e9dcb77c96d3f
Michael Nixon <michael@edukits.co>
2026-02-26 09:15:31 -0800

    feat: add unsat test example

--- tests/test_translations.py
+++ tests/test_translations.py
@@ -2,10 +2,22 @@ from pysmt.shortcuts import is_sat, is_unsat
 from pysmt.smtlib.parser import get_formula
 from io import StringIO
 
+SOLVER_NAME = "cvc5"
+
 def test_sat():
+    # x in Nat, x > 0
     smtlib_content = """
     (declare-const x Nat)
     (assert (> x 0))
     """
     formula = get_formula(StringIO(smtlib_content))
-    assert is_sat(formula, solver_name="cvc5")
+    assert is_sat(formula, solver_name=SOLVER_NAME)
+
+def test_unsat():
+    # x in Nat, x < 0
+    smtlib_content = """
+    (declare-const x Nat)
+    (assert (< x 0))
+    """
+    formula = get_formula(StringIO(smtlib_content))
+    assert is_unsat(formula, solver_name=SOLVER_NAME)
